424f85b284b2de04131b798c8a82b8fcb114ce2c
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-16 13:09:56 +0000

    copied program

--- .ccls-cache/@home@runner@Ch13Q1P1-introducing-define-statement/main.c
+++ .ccls-cache/@home@runner@Ch13Q1P1-introducing-define-statement/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+#define YES 1
+#define NO 0
+
+// Function to determine if an integer is even
+int isEven(int);
+
 int main(void) {
-  printf("Hello World\n");
+
+  if (isEven (17) == YES)
+    printf ("yes ");
+  else
+    printf ("no ");
+
+  if (isEven (20) == YES)
+    printf ("yes\n");
+  else
+    printf ("no\n");
+  
   return 0;
+}
+
+// Define Functions
+int isEven (int number) {
+  int answer;
+
+  if (number % 2 == 0)
+    answer = YES;
+  else 
+    answer = NO;
+
+  return answer;
 }
